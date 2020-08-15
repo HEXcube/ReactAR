e9a8d78f3a727b18a7645040abc306093ea69fc6
HEXcube <hexcubed@gmail.com>
2020-08-16 02:24:32 +0530

    Updates

---
+++ precache-manifest.2d8688892aa22a0bf048b3f2bebedab0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8982cd69b042945e309cc482b758b67e",
+    "url": "/ReactAR/index.html"
+  },
+  {
+    "revision": "3d13cfe9efb571f702fb",
+    "url": "/ReactAR/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e763542bc6a33c299fcf",
+    "url": "/ReactAR/static/js/2.a0e23192.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactAR/static/js/2.a0e23192.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d13cfe9efb571f702fb",
+    "url": "/ReactAR/static/js/main.7d5aff1a.chunk.js"
+  },
+  {
+    "revision": "c131ac5301186b87869c",
+    "url": "/ReactAR/static/js/runtime-main.8442f137.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactAR/static/media/logo.5d5d9eef.svg"
+  }
+]);
