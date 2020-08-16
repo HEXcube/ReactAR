43f7be263f6f4bfcb878b6c120837bf8935ca25d
HEXcube <hexcubed@gmail.com>
2020-08-16 19:28:00 +0530

    Port EnchantAR to React

--- src/App.js
+++ src/App.js
@@ -2,25 +2,44 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="arjs-loader">
+          <div className="arjs-loader-spinner"></div>
+        </div>
+
+        <div className="fab">
+            <span className="fab-action-button">
+                <i className="fab-icon"></i>
+            </span>
+            <ul className="fab-buttons">
+                <li className="fab-buttons-item">
+                    <a className="fab-buttons-link" target="_blank" href="https://github.com/HEXcube/EnchantAR/blob/master/README.md" data-tooltip="How to use">
+                        <i className="icon-material icon-material-info"></i>
+                    </a>
+                </li>
+                <li className="fab-buttons-item">
+                    <a className="fab-buttons-link" target="_blank" href="https://github.com/HEXcube/EnchantAR/raw/master/assets/markers/phoenix-of-autumn.jpg" data-tooltip="Open marker image">
+                        <i className="icon-material icon-material-image"></i>
+                    </a>
+                </li>
+                <li className="fab-buttons-item">
+                    <a className="fab-buttons-link" target="_blank" href="https://github.com/HEXcube/EnchantAR" data-tooltip="View source code">
+                        <i className="icon-material icon-material-source"></i>
+                    </a>
+                </li>
+                <li className="fab-buttons-item">
+                    <a className="fab-buttons-link" target="_blank" href="https://hexcube.github.io" data-tooltip="About me">
+                        <i className="icon-material icon-material-about"></i>
+                    </a>
+                </li>
+            </ul>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
